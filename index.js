b65b35aff79e54bad1dbf5959a5a04b15ae6d31d
Subrata34 <subratamodak3491@gmail.com>
2022-03-03 13:22:07 +0600

    files

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const express = require('express')
-const app = express()
-const  cors=require('cors');
-const fileUpload = require('express-fileupload');
-require('dotenv').config();
-const { MongoClient, ConnectionCheckedInEvent } = require('mongodb');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 
-
-const port = process.env.PORT ||5000;
-
+const port = process.env.PORT || 5000;
 
 //midle wire
 app.use(express.json());
@@ -16,32 +14,33 @@ app.use(fileUpload());
 
 //mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4aj0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 console.log(uri);
 
-async function run(){
-    try{
-        await client.connect();
-        const database=client.db('buyer');
-        const buyerCollection=database.collection('user');
-
-        // app.post('/user',async(req,res)=>{
-            
-        // })
-    }
-    finally{
-
-    }
-
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('tourplanner');
+    const buyerCollection = database.collection('place');
+ 
+    //get api
+    app.get("/place", async (req, res) => {
+      const couser= buyerCollection.find({});
+      const place=await couser.toArray();
+      res.send(place);
+    });
+  } finally {
+  }
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(` listening on port ${port}`)
-})
+  console.log(` listening on port ${port}`);
+});
